709c2e4382edf9931b96bfd311eba37c5c9b0d8b
hoang <vinhyenvodoi98@gmail.com>
2020-08-26 10:57:24 +0700

    feat : connect multi database mongodb and postgres

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 import { config } from './ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config)],
+  imports: [
+    TypeOrmModule.forRoot(config),
+    MongooseModule.forRoot('mongodb://mongo:27017/hydroData'),
+  ],
   controllers: [],
   providers: [],
 })
